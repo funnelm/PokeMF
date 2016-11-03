12aa94d01ac66ed37c130ff0e973bf1aed005b30
funnelm <markfunnell@Marks-MacBook-Pro.local>
2016-11-03 15:01:28 -0400

    Initial Commit

---
+++ PokeMF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeMF
+//
+//  Created by Mark Funnell on 11/3/16.
+//  Copyright © 2016 Mark Funnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
